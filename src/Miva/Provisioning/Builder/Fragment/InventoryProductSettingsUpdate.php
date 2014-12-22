b820f7c21c9303f93b9abb74331a188149794896
Gassan Idriss <gidriss@mivamerchant.com>
2014-12-22 14:04:05 -0800

    fixes

--- src/Miva/Provisioning/Builder/Fragment/InventoryProductSettingsUpdate.php
+++ src/Miva/Provisioning/Builder/Fragment/InventoryProductSettingsUpdate.php
@@ -14,120 +14,71 @@ use Miva\Provisioning\Builder\Helper\XmlHelper;
 use Miva\Provisioning\Builder\SimpleXMLElement;
 
 /**
-* InventoryProductSettingsUpdate
-*
-* @author Gassan Idriss <gidriss@mivamerchant.com>
-*/
+ * InventoryProductSettingsUpdate
+ *
+ * @author Gassan Idriss <gidriss@mivamerchant.com>
+ */
 class InventoryProductSettingsUpdate implements Model\StoreFragmentInterface
 {
+
+    const VALUE_DEFAULT = 'default';
+
     /** @var string */
     public $productCode;
-    
+
     /** @var boolean */
     public $trackProduct = false;
-    
+
     /** @var int */
     public $adjustStockBy;
-    
+
     /** @var string */
     public $inStockMessageShort;
-    
+
     /** @var string */
     public $inStockMessageLong;
-    
+
     /** @var boolean */
-    public $trackLowStockLevel = false;
-    
+    public $trackLowStockLevel = self::VALUE_DEFAULT;
+
     /** @var string */
-    public $lowStockLevel;
-    
+    public $lowStockLevel = self::VALUE_DEFAULT;
+
     /** @var string */
     public $lowStockMessageShort;
-    
+
     /** @var string */
     public $lowStockMessageLong;
-    
+
     /** @var boolean */
-    public $trackOutOfStockLevel = false;
-    
+    public $trackOutOfStockLevel = self::VALUE_DEFAULT;
+
     /** @var string */
-    public $hideOutOfStockProducts = false;
-    
+    public $hideOutOfStockProducts = self::VALUE_DEFAULT;
+
     /** @var string */
-    public $outOfStockLevel;
-    
+    public $outOfStockLevel = self::VALUE_DEFAULT;
+
     /** @var string */
     public $outOfStockMessageShort;
-    
+
     /** @var string */
     public $outOfStockMessageLong;
-    
+
     /** @var string */
     public $limitedStockMessage;
-    
-    /**
-     * getProductCode
-     *
-     * @return string
-    */
-    public function getProductCode()
-    {
-        return $this->productCode;
-    }
-    
-    /**
-     * setProductCode
-     *
-     * @param string $productCode
-     *
-     * @return self
-    */
-    public function setProductCode($productCode)
-    {
-        $this->productCode = $productCode;
-        return $this;
-    }
 
     /**
-     * getTrackProduct
-     *
-     * @return boolean
-    */
-    public function getTrackProduct()
-    {
-        return $this->trackProduct;
-    }
-    
-    /**
-     * setTrackProduct
-     *
-     * @param boolean $trackProduct
-     *
-     * @return self
-    */
-    public function setTrackProduct($trackProduct)
-    {
-        $this->trackProduct = $trackProduct;
-        return $this;
-    }
-
-    /**
-     * getAdjustStockBy
-     *
      * @return int
-    */
+     */
     public function getAdjustStockBy()
     {
         return $this->adjustStockBy;
     }
-    
+
     /**
-     * setAdjustStockBy
-     *
      * @param int $adjustStockBy
-     *
-     * @return self
-    */
+     */
     public function setAdjustStockBy($adjustStockBy)
     {
         $this->adjustStockBy = $adjustStockBy;
@@ -135,114 +86,118 @@ class InventoryProductSettingsUpdate implements Model\StoreFragmentInterface
     }
 
     /**
-     * getInStockMessageShort
-     *
      * @return string
-    */
-    public function getInStockMessageShort()
+     */
+    public function getHideOutOfStockProducts()
     {
-        return $this->inStockMessageShort;
+        return $this->hideOutOfStockProducts;
     }
-    
+
     /**
-     * setInStockMessageShort
-     *
-     * @param string $inStockMessageShort
-     *
-     * @return self
-    */
-    public function setInStockMessageShort($inStockMessageShort)
+     * @param string $hideOutOfStockProducts
+     */
+    public function setHideOutOfStockProducts($hideOutOfStockProducts)
     {
-        $this->inStockMessageShort = $inStockMessageShort;
+        $this->hideOutOfStockProducts = $hideOutOfStockProducts;
         return $this;
     }
-    
+
     /**
-     * getInStockMessageLong
-     *
      * @return string
-    */
+     */
     public function getInStockMessageLong()
     {
         return $this->inStockMessageLong;
     }
-    
+
     /**
-     * setInStockMessageLong
-     *
      * @param string $inStockMessageLong
-     *
-     * @return self
-    */
+     */
     public function setInStockMessageLong($inStockMessageLong)
     {
         $this->inStockMessageLong = $inStockMessageLong;
         return $this;
     }
-    
+
     /**
-     * getLowStockLevel
-     *
      * @return string
-    */
+     */
+    public function getInStockMessageShort()
+    {
+        return $this->inStockMessageShort;
+    }
+
+    /**
+     * @param string $inStockMessageShort
+     */
+    public function setInStockMessageShort($inStockMessageShort)
+    {
+        $this->inStockMessageShort = $inStockMessageShort;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLimitedStockMessage()
+    {
+        return $this->limitedStockMessage;
+    }
+
+    /**
+     * @param string $limitedStockMessage
+     */
+    public function setLimitedStockMessage($limitedStockMessage)
+    {
+        $this->limitedStockMessage = $limitedStockMessage;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
     public function getLowStockLevel()
     {
         return $this->lowStockLevel;
     }
-    
+
     /**
-     * setLowStockLevel
-     *
      * @param string $lowStockLevel
-     *
-     * @return self
-    */
+     */
     public function setLowStockLevel($lowStockLevel)
     {
         $this->lowStockLevel = $lowStockLevel;
         return $this;
     }
-    
+
     /**
-     * getTrackLowStockLevel
-     *
-     * @return boolean
-    */
-    public function getTrackLowStockLevel()
+     * @return string
+     */
+    public function getLowStockMessageLong()
     {
-        return $this->trackLowStockLevel;
+        return $this->lowStockMessageLong;
     }
-    
+
     /**
-     * setTrackLowStockLevel
-     *
-     * @param boolean $trackLowStockLevel
-     *
-     * @return self
-    */
-    public function setTrackLowStockLevel($trackLowStockLevel)
+     * @param string $lowStockMessageLong
+     */
+    public function setLowStockMessageLong($lowStockMessageLong)
     {
-        $this->trackLowStockLevel = $trackLowStockLevel;
+        $this->lowStockMessageLong = $lowStockMessageLong;
         return $this;
     }
 
     /**
-     * getLowStockMessageShort
-     *
      * @return string
-    */
+     */
     public function getLowStockMessageShort()
     {
         return $this->lowStockMessageShort;
     }
-    
+
     /**
-     * setLowStockMessageShort
-     *
      * @param string $lowStockMessageShort
-     *
-     * @return self
-    */
+     */
     public function setLowStockMessageShort($lowStockMessageShort)
     {
         $this->lowStockMessageShort = $lowStockMessageShort;
@@ -250,81 +205,130 @@ class InventoryProductSettingsUpdate implements Model\StoreFragmentInterface
     }
 
     /**
-     * getLowStockMessageLong
-     *
      * @return string
-    */
-    public function getLowStockMessageLong()
+     */
+    public function getOutOfStockLevel()
     {
-        return $this->lowStockMessageLong;
+        return $this->outOfStockLevel;
     }
-    
+
     /**
-     * setLowStockMessageLong
-     *
-     * @param string $lowStockMessageLong
-     *
-     * @return self
-    */
-    public function setLowStockMessageLong($lowStockMessageLong)
+     * @param string $outOfStockLevel
+     */
+    public function setOutOfStockLevel($outOfStockLevel)
     {
-        $this->lowStockMessageLong = $lowStockMessageLong;
+        $this->outOfStockLevel = $outOfStockLevel;
         return $this;
     }
-    
+
     /**
-     * getTrackOutOfStockLevel
-     *
+     * @return string
+     */
+    public function getOutOfStockMessageLong()
+    {
+        return $this->outOfStockMessageLong;
+    }
+
+    /**
+     * @param string $outOfStockMessageLong
+     */
+    public function setOutOfStockMessageLong($outOfStockMessageLong)
+    {
+        $this->outOfStockMessageLong = $outOfStockMessageLong;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOutOfStockMessageShort()
+    {
+        return $this->outOfStockMessageShort;
+    }
+
+    /**
+     * @param string $outOfStockMessageShort
+     */
+    public function setOutOfStockMessageShort($outOfStockMessageShort)
+    {
+        $this->outOfStockMessageShort = $outOfStockMessageShort;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProductCode()
+    {
+        return $this->productCode;
+    }
+
+    /**
+     * @param string $productCode
+     */
+    public function setProductCode($productCode)
+    {
+        $this->productCode = $productCode;
+        return $this;
+    }
+
+    /**
      * @return boolean
-    */
+     */
+    public function getTrackLowStockLevel()
+    {
+        return $this->trackLowStockLevel;
+    }
+
+    /**
+     * @param boolean $trackLowStockLevel
+     */
+    public function setTrackLowStockLevel($trackLowStockLevel)
+    {
+        $this->trackLowStockLevel = $trackLowStockLevel;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
     public function getTrackOutOfStockLevel()
     {
         return $this->trackOutOfStockLevel;
     }
-    
+
     /**
-     * setTrackOutOfStockLevel
-     *
      * @param boolean $trackOutOfStockLevel
-     *
-     * @return self
-    */
+     */
     public function setTrackOutOfStockLevel($trackOutOfStockLevel)
     {
         $this->trackOutOfStockLevel = $trackOutOfStockLevel;
         return $this;
     }
-    
+
     /**
-     * getHideOutOfStockProducts
-     *
      * @return boolean
-    */
-    public function getHideOutOfStockProducts()
+     */
+    public function getTrackProduct()
     {
-        return $this->hideOutOfStockProducts;
+        return $this->trackProduct;
     }
-    
+
     /**
-     * setHideOutOfStockProducts
-     *
-     * @param boolean $hideOutOfStockProducts
-     *
-     * @return self
-    */
-    public function setHideOutOfStockProducts($hideOutOfStockProducts)
+     * @param boolean $trackProduct
+     */
+    public function setTrackProduct($trackProduct)
     {
-    	$this->hideOutOfStockProducts = $hideOutOfStockProducts;
+        $this->trackProduct = $trackProduct;
         return $this;
     }
 
 
-
     /**
      * {@inheritDoc}
-     * 
+     *
      * Format:
-     * 
+     *
      * <InventoryProductSettings_Update product_code="ale-gallon">
      *     <TrackProduct>Yes</TrackProduct>
      *     <AdjustStockBy>500</AdjustStockBy>
@@ -341,30 +345,62 @@ class InventoryProductSettingsUpdate implements Model\StoreFragmentInterface
      *     <OutOfStockMessageLong><![CDATA[]]></OutOfStockMessageLong>
      *     <LimitedStockMessage><![CDATA[]]></LimitedStockMessage>
      * </InventoryProductSettings_Update>
-    */
+     */
     public function toXml($version = Version::CURRENT, array $options = array())
     {
 
-        $xmlObject = new SimpleXMLElement('<InventoryProductSettings_Update></InventoryProductSettings_Update>');
-        
+        $booleanValues = array('yes','no', '1', '0');
+
+        $xmlObject = new SimpleXMLElement('<InventoryProductSettings_Update />');
+
         $xmlObject->addAttribute('product_code', $this->getProductCode());
-        
+
         $xmlObject->addChild('TrackProduct', $this->getTrackProduct() ? 'Yes' : 'No');
         $xmlObject->addChild('AdjustStockBy', $this->getAdjustStockBy());
-        $xmlObject->addChild('InStockMessageShort', $this->getInStockMessageShort())->addAttribute('method-call', 'addCDATA');;
-        $xmlObject->addChild('InStockMessageLong', $this->getInStockMessageLong())->addAttribute('method-call', 'addCDATA');;
-        $xmlObject->addChild('TrackLowStockLevel', $this->getTrackLowStockLevel() ? 'Yes' : 'No');
-        $xmlObject->addChild('LowStockLevel', $this->getLowStockLevel());
-        $xmlObject->addChild('LowStockMessageShort', $this->getLowStockMessageShort())->addAttribute('method-call', 'addCDATA');;
-        $xmlObject->addChild('LowStockMessageLong', $this->getLowStockMessageLong())->addAttribute('method-call', 'addCDATA');;
-        $xmlObject->addChild('TrackOutOfStockLevel', $this->getTrackOutOfStockLevel() ? 'Yes' : 'No');
-        $xmlObject->addChild('HideOutOfStockProducts', $this->getHideOutOfStockProducts() ? 'Yes' : 'No');
-        $xmlObject->addChild('OutOfStockLevel', $this->getOutOfStockLevel());
-        $xmlObject->addChild('OutOfStockMessageShort', $this->getOutOfStockMessageShort())->addAttribute('method-call', 'addCDATA');;
-        $xmlObject->addChild('OutOfStockMessageLong', $this->getOutOfStockMessageLong())->addAttribute('method-call', 'addCDATA');;
-        $xmlObject->addChild('LimitedStockMessage', $this->getLimitedStockMessage())->addAttribute('method-call', 'addCDATA');;
+
+        $xmlObject->addChild('InStockMessageShort', $this->getInStockMessageShort())->addAttribute('method-call', 'addCDATA');
+        $xmlObject->addChild('InStockMessageLong', $this->getInStockMessageLong())->addAttribute('method-call', 'addCDATA');
+
+        if (is_bool($this->getTrackLowStockLevel()) || in_array(strtolower($this->getTrackLowStockLevel()),  $booleanValues)) {
+            $xmlObject->addChild('TrackLowStockLevel', $this->getTrackLowStockLevel() ? 'Yes' : 'No');
+        } else {
+            $xmlObject->addChild('TrackLowStockLevel', static::VALUE_DEFAULT);
+        }
+
+        if (is_numeric($this->getLowStockLevel())) {
+            $xmlObject->addChild('LowStockLevel', $this->getLowStockLevel());
+        } else {
+            $xmlObject->addChild('LowStockLevel', static::VALUE_DEFAULT);
+        }
+
+
+        $xmlObject->addChild('LowStockMessageShort', $this->getLowStockMessageShort())->addAttribute('method-call', 'addCDATA');
+        $xmlObject->addChild('LowStockMessageLong', $this->getLowStockMessageLong())->addAttribute('method-call', 'addCDATA');
+
+        if (is_bool($this->getTrackOutOfStockLevel()) || in_array(strtolower($this->getTrackOutOfStockLevel()), $booleanValues)) {
+            $xmlObject->addChild('TrackOutOfStockLevel', $this->getTrackOutOfStockLevel() ? 'Yes' : 'No');
+        } else {
+            $xmlObject->addChild('TrackOutOfStockLevel', static::VALUE_DEFAULT);
+        }
+
+        if (is_bool($this->getHideOutOfStockProducts()) || in_array(strtolower($this->getHideOutOfStockProducts()), $booleanValues)) {
+            $xmlObject->addChild('HideOutOfStockProducts', $this->getHideOutOfStockProducts() ? 'Yes' : 'No');
+        } else {
+            $xmlObject->addChild('HideOutOfStockProducts', static::VALUE_DEFAULT);
+        }
+
+        if (is_numeric($this->getOutOfStockLevel())) {
+            $xmlObject->addChild('OutOfStockLevel', $this->getOutOfStockLevel());
+        } else {
+            $xmlObject->addChild('OutOfStockLevel', static::VALUE_DEFAULT);
+        }
+
+        $xmlObject->addChild('OutOfStockMessageShort', $this->getOutOfStockMessageShort())->addAttribute('method-call', 'addCDATA');
+        $xmlObject->addChild('OutOfStockMessageLong', $this->getOutOfStockMessageLong())->addAttribute('method-call', 'addCDATA');
+        $xmlObject->addChild('LimitedStockMessage', $this->getLimitedStockMessage())->addAttribute('method-call', 'addCDATA');
 
         return $xmlObject;
     }
+
 }
         
--- src/Miva/Provisioning/Builder/Fragment/OrderAddItem.php
+++ src/Miva/Provisioning/Builder/Fragment/OrderAddItem.php
@@ -1,34 +1,243 @@
-<Order_Add_Item order_id="1000">                                (Required - order_id)
-            <Code>test</Code>
-            <Name>Test Product #1</Name>
-            <Price>1</Price>
-            <Weight>0</Weight>
-            <Quantity>1</Quantity>
-            
-            <Options>
-                <Option>
-                    <AttributeCode>test</AttributeCode>
-                </Option>
+<?php
+/*
+* This file is part of the Miva PHP Provision package.
+*
+* (c) Gassan Idriss <gidriss@mivamerchant.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace Miva\Provisioning\Builder\Fragment;
 
-                <Option>
-                    <AttributeCode>template_attr</AttributeCode>
-                    <Price>1.00</Price>
-                    <OptionCode>v1</OptionCode>
-                </Option>
-            </Options>
-            
-            <Shipment>                                                  (Optional)
-                <Code>Ship_Code</Code>                                  (Optional)
-                <Cost>1</Cost>                                          (Optional)
-                <MarkAsShipped>Yes</MarkAsShipped>                      (Optional)
-                <TrackingNumber>1111111111</TrackingNumber>             (Required)
-                <TrackingType>FedEx</TrackingType>                      (Required)
-                <ShipDate>                                              (Optional)
-                    <Day>7</Day>
-                    <Month>9</Month>
-                    <Year>2012</Year>
-                </ShipDate>
-            </Shipment>
-            
-            <Shipment />                                                (Optional)
-        </Order_Add_Item>
+use Miva\Version;
+use Miva\Provisioning\Builder\Helper\XmlHelper;
+use Miva\Provisioning\Builder\SimpleXMLElement;
+
+/**
+ * OrderAddItem
+ *
+ * @author Gassan Idriss <gidriss@mivamerchant.com>
+ */
+class OrderAddItem implements Model\StoreFragmentInterface
+{
+
+    public $orderId;
+
+    public $code;
+
+    public $name;
+
+    public $price;
+
+    public $weight;
+
+    public $quantity;
+
+    public $options = array();
+
+    public $shipments = array();
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrderId()
+    {
+        return $this->orderId;
+    }
+
+    /**
+     * @param mixed $orderId
+     */
+    public function setOrderId($orderId)
+    {
+        $this->orderId = $orderId;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * @param mixed $quantity
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+
+    /**
+     * @param mixed $weight
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setOptions(array $options = array())
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+    /**
+     * @param OrderItemOption $option
+     * @return $this
+     */
+    public function addOption(OrderItemOption $option)
+    {
+        $this->options[] = $option;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getShipments()
+    {
+        return $this->shipments;
+    }
+
+    /**
+     * @param array $shipments
+     */
+    public function setShipments(array $shipments = array())
+    {
+        $this->shipments = $shipments;
+        return $this;
+    }
+
+    /**
+     * @param OrderItemShipment $shipment
+     */
+    public function addShipment(OrderItemShipment $shipment)
+    {
+        $this->shipments[] = $shipment;
+        return $this;
+    }
+
+
+
+    /**
+     * {@inheritDoc}
+     *
+     * Format:
+     *
+     * <Order_Add_Item order_id="1000">                                (Required - order_id)
+     *  <Code>test</Code>
+     *  <Name>Test Product #1</Name>
+     *  <Price>1</Price>
+     *  <Weight>0</Weight>
+     *  <Quantity>1</Quantity>
+     *  <Options>
+     *      <Option>
+     *          <AttributeCode>test</AttributeCode>
+     *      </Option>
+     *      <Option>
+     *          <AttributeCode>template_attr</AttributeCode>
+     *          <Price>1.00</Price>
+     *          <OptionCode>v1</OptionCode>
+     *      </Option>
+     *  </Options>
+
+     *  <Shipment>                                                  (Optional)
+     *      <Code>Ship_Code</Code>                                  (Optional)
+     *      <Cost>1</Cost>                                          (Optional)
+     *      <MarkAsShipped>Yes</MarkAsShipped>                      (Optional)
+     *      <TrackingNumber>1111111111</TrackingNumber>             (Required)
+     *      <TrackingType>FedEx</TrackingType>                      (Required)
+     *      <ShipDate>                                              (Optional)
+     *          <Day>7</Day>
+     *          <Month>9</Month>
+     *          <Year>2012</Year>
+     *      </ShipDate>
+     *  </Shipment>
+     *  <Shipment />                                                (Optional)
+     * </Order_Add_Item>
+     */
+    public function toXml($version = Version::CURRENT, array $options = array())
+    {
+        $xmlObject = new SimpleXMLElement('<Order_Add_Item />');
+
+
+
+        return $xmlObject;
+    }
+}
+
